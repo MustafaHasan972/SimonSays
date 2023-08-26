15e16a8e7cf8e907dc502a09b7cf40988bf1b613
Mustafa Hasan <mustafa2k8972@gmail.com>
2023-08-26 13:22:48 +0530

    Added h3 tag for high score

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ let btns = ["yellow", "green", "red", "purple"];
 let started = false;
 let level = 0;
 
+let count = 0;
+
 let h2 = document.querySelector("h2");
 
 document.addEventListener("keypress", function (e) {
@@ -20,6 +22,7 @@ function levelUp() {
   userSeq = [];
   level++;
   h2.innerText = `Level ${level}`;
+  count = level;
 
   let randIdx = Math.floor(Math.random() * 3);
   let randColor = btns[randIdx];
